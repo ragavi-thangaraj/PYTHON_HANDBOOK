169197e870fd7246424701415aefb136d7aeef3c
ragavi <ragavithangaraj09@gmail.com>
2025-12-06 21:03:10 +0530

    Multi Line Input

---
+++ BASICS/multiline_input.py
@@ -0,0 +1,11 @@
+#just get the input with a split function
+a,b,c=map(int,input("Enter the values : ").split()) # give input like 10 <space> 20 <space> 30
+print(a)
+print(b)
+print(c)
+
+# Sample output
+# Enter the values : 10 20 30
+# 10
+# 20
+# 30
