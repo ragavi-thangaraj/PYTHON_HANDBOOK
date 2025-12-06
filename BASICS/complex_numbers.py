8c499bf1a11ec5bcc2ee10cc7f75ba104886eda5
ragavi <ragavithangaraj09@gmail.com>
2025-12-06 21:48:47 +0530

    Complex Numbers

---
+++ BASICS/complex_numbers.py
@@ -0,0 +1,13 @@
+# -------------------------------
+# Two ways to create complex numbers in Python
+# -------------------------------
+
+# Method 1: Directly using real + imaginary part with 'j'
+complex_number = 2 + 6j
+print("Method 1 Output:", complex_number)  
+# Sample Output: (2+6j)
+
+# Method 2: Using the built-in complex() constructor → complex(real, imaginary)
+z = complex(5, 6)     # Represents 5 + 6j
+print("Method 2 Output:", z)
+# Sample Output: (5+6j)
